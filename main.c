c46c58d297ac55f4178c50b4e3cee1cbe6e7cb2d
SantipapV <92297179+SantipapV@users.noreply.github.com>
2021-10-22 19:08:01 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, Wisdoms!");
+   return 0;
+}
+
